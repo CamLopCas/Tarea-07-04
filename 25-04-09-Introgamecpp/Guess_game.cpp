f839553ea0798a262127789c7af2f994c7504241
LOPEZ CASTILLO CAMILO ANDRES <camlopez@unal.edu.co>
2025-04-09 07:36:44 -0500

    Subi Guess_game.cpp

---
+++ 25-04-09-Introgamecpp/Guess_game.cpp
@@ -0,0 +1,16 @@
+/*JUego de adivinar un numero
+Tener un numero
+Preguntar al usuario un numero
+Informar si lo adivinio o no
+*/
+
+#include <iostream>
+
+int main(void){
+    const int NUM = 10;
+    int guesses_number;
+    std::cout << "Adivina un numero entre 1 y 100:\n";
+    std::cin >> guesses_number;
+
+    return 0;
+}
