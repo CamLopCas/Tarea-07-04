3ac66ec56e7375be47c831b7997f3a4bcfe36390
LOPEZ CASTILLO CAMILO ANDRES <camlopez@unal.edu.co>
2025-04-23 08:50:42 -0500

    Se añade Cousin_primescmd.cpp

---
+++ 25-04-23-Funcionescpp/Cousin_primescmd.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath>
+
+// declaration
+bool isprime(long n);
+void test(void);
+
+int main(int argc, char **argv){
+    
+    int vimn = std::stoi(argv[1]);
+    int vmax = std::stoi(argv[2]);
+
+
+    for(int ii = vimn; ii <= vmax; ++ii) {
+        if (isprime(ii) == true) {
+            if(isprime(ii) == true and isprime(ii+6)==true){
+            std::cout << "(" << ii  << ", " << ii+6 << ")\n";
+            }
+        }
+    }
+    
+    return 0;
+}
+
+// implementation
+bool isprime(long n) {
+    // precondition checks
+    // do something if n <= 1
+    if (n <= 1) {
+        std::cerr << "Out of range number: " << n << "\n";
+        return false;
+    }
+
+    // suppose it is prime
+    bool flag = true; 
+
+    if (n == 2) {
+        return true;
+    }
+
+    // find divisors
+    for (long ii = 2; ii <= std::sqrt(n) + 1; ii++) {
+        if (n%ii == 0) { // ii is divisor
+            flag = false;
+            break; // end, at least one divisor
+        }
+    }
+
+    return flag;
+}
+
+void test(void){
+    //std::cout << 1 << ": " << isprime(1) << "\n";
+    std::cout << 2 << ": " << isprime(2) << "\n";
+    std::cout << 15 << ": " << isprime(15) << "\n";
+    std::cout << 10000000019 << ": " << isprime(10000000019) << "\n";
+    std::cout << 1000000007 << ": " << isprime(1000000007) << "\n";
+}
